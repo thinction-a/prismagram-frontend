1bcfc286a882e81b22aa394bb73969caa00535c0
thinction-cmo <jakuro94@gmail.com>
2020-06-05 17:57:46 +0900

    #7.3 start

--- src/Components/Routes.js
+++ src/Components/Routes.js
@@ -4,17 +4,26 @@ import { Route, Switch } from "react-router-dom";
 import Auth from "../Routes/Auth";
 import Feed from "../Routes/Feed";
 import Explore from "../Routes/Explore";
+import Search from "../Routes/Search";
 import Profile from "../Routes/Profile";
 
-const LoggedInRoutes = () => <><Route exact path="/" component={Feed} /></>
+const LoggedInRoutes = () => (
+    <Switch>
+        <Route exact path="/" component={Feed} />
+        <Route path="/explore" component={Explore} />
+        <Route path="/search" component={Search} />
+        <Route path="/:username" component={Profile} />
+    </Switch>
+);
 
-const LoggedOutRoutes = () => <><Route exact path="/" component={Auth} /></>
+const LoggedOutRoutes = () => (
+    <Switch>
+        <Route exact path="/" component={Auth} />
+    </Switch>
+);
 
 const AppRouter = ({ isLoggedIn }) => (
-    // <Router>
-    //     <Switch>{isLoggedIn ? <LoggedInRoutes /> : <LoggedOutRoutes /> }</Switch>
-    // </Router>
-    <Switch>{isLoggedIn ? <LoggedInRoutes /> : <LoggedOutRoutes />}</Switch>
+    isLoggedIn ? <LoggedInRoutes /> : <LoggedOutRoutes />
 );
 
 AppRouter.propTypes = {
